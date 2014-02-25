a1900c001c49d7ebefe29a2a72f21166012ca19e
larrylisky <larrylisky@gmail.com>
2014-02-24 21:35:51 -0600

    Separated out Settings class

---
+++ settings.hpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/highgui.hpp>
+
+#ifndef __SETTINGS_HPP__
+#define __SETTINGS_HPP__
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+using namespace cv;
+using namespace std;
+
+class Settings
+{
+public:
+    Settings() : goodInput(false) {}
+    enum Pattern { NOT_EXISTING, CHESSBOARD, CIRCLES_GRID, ASYMMETRIC_CIRCLES_GRID };
+    enum InputType { INVALID, CAMERA, VIDEO_FILE, IMAGE_LIST };
+
+    void write(FileStorage& fs) const;                       //Write serialization for this class
+    void read(const FileNode& node);                          //Read serialization for this class
+    void interprate();
+    Mat nextImage();
+    static bool readStringList( const string& filename, vector<string>& l );
+    
+public:
+    Size boardSize;            // The size of the board -> Number of items by width and height
+    Pattern calibrationPattern;// One of the Chessboard, circles, or asymmetric circle pattern
+    float squareSize;          // The size of a square in your defined unit (point, millimeter,etc).
+    int nrFrames;              // The number of frames to use from the input for calibration
+    float aspectRatio;         // The aspect ratio
+    int delay;                 // In case of a video input
+    bool bwritePoints;         //  Write detected feature points
+    bool bwriteExtrinsics;     // Write extrinsic parameters
+    bool calibZeroTangentDist; // Assume zero tangential distortion
+    bool calibFixPrincipalPoint;// Fix the principal point at the center
+    bool flipVertical;          // Flip the captured images around the horizontal axis
+    string outputFileName;      // The name of the file where to write
+    bool showUndistorsed;       // Show undistorted images after calibration
+    string input;               // The input ->
+
+    int cameraID;
+    vector<string> imageList;
+    int atImageList;
+    VideoCapture inputCapture;
+    InputType inputType;
+    bool goodInput;
+    int flag;
+
+private:
+    string patternToUse;
+};
+
+#endif     // __SETTINGS_HPP__ 
